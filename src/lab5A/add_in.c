b4e16e759520a1d2df02024c9d37c1767e8bf220
johnvfranco <franco@gauss.ececs.uc.edu>
2023-03-06 13:54:05 -0500

    add

--- src/lab5A/add_in.c
+++ src/lab5A/add_in.c
@@ -1,5 +1,15 @@
 #include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 void add_in(uint32_t *x, uint32_t y) {
     *x += y;
 }
+
+int main (int argc, char** argv) {
+	uint32_t a = atol(argv[1]);
+	uint32_t b = atol(argv[2]);	
+	add_in(&a,b);
+	printf("%u\n",a);
+}
+
