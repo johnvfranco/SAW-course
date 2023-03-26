324c2a3d1961aecd56b0a482649e0eec96242ee4
johnvfranco <franco@gauss.ececs.uc.edu>
2023-03-26 06:53:07 -0400

    add

---
+++ src/lab5B/ffs_ref.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef unsigned int uint32_t;
+
+uint32_t ffs_ref(uint32_t word) {
+   int i = 0;
+   int cnt = 0;
+   if (!word) return 0;
+   for (cnt = 0; cnt < 32; cnt++)
+      if (((1 << i++) & word) != 0) return i;
+   return 0;
+}
+
+int main (int argc, char **argv) {
+   if (argc != 2) {
+      printf("Usage: %s <number>\n", argv[0]);
+      exit(0);
+   }
+   uint32_t n = atol(argv[1]);
+	uint32_t m = ffs_ref(n);
+   if (m == 0) printf("ffs_ref: no 1s in this number\n");
+	else printf("ffs_ref: first 1 is at %d\n", m);
+}
