02dea8946b8a0f2be0a8d1380d6376f94655e347
johnvfranco <franco@gauss.ececs.uc.edu>
2024-02-01 07:53:04 -0500

    add

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export PATH=`pwd`:$HOME/.bin:`pwd`/src:`pwd`/bin:/usr/bin:bin
 export CD=`pwd`
-java -Dsun.java2d.uiScale=1.0 -jar game.jar &
+java -Dsun.java2d.uiScale=1.0 -jar game.jar 
 
